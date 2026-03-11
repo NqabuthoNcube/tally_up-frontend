71f1b8045bbef4947f6b6919488ba77fdefae13a
Nqabutho Ncube <nqabuthoncube15@gmail.com>
2026-03-11 14:54:54 +0530

    update

--- lib/features/profile/help.dart
+++ lib/features/profile/help.dart
@@ -1,33 +1,46 @@
 import 'package:flutter/material.dart';
 
 class HelpScreen extends StatelessWidget {
+
   const HelpScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+
       appBar: AppBar(
         title: const Text("Help & Support"),
       ),
+
       body: ListView(
-        children: const [
-          SizedBox(height: 20),
-          ListTile(
+
+        children: [
+
+          const SizedBox(height: 20),
+
+          const ListTile(
             leading: Icon(Icons.help_outline),
             title: Text("How to send Tally"),
           ),
-          Divider(),
-          ListTile(
+
+          const Divider(),
+
+          const ListTile(
             leading: Icon(Icons.qr_code),
             title: Text("Using QR payments"),
           ),
-          Divider(),
-          ListTile(
+
+          const Divider(),
+
+          const ListTile(
             leading: Icon(Icons.security),
             title: Text("Security tips"),
           ),
-          Divider(),
-          ListTile(
+
+          const Divider(),
+
+          const ListTile(
             leading: Icon(Icons.contact_support),
             title: Text("Contact support"),
           ),
